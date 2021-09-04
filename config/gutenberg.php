d610dfc086a0c6640bece4519892bf35b101f301
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-09-04 17:59:18 +0430

    Added configuration

---
+++ config/gutenberg.php
@@ -0,0 +1,49 @@
+<?php 
+
+use Zareismail\Gutenberg\Models\GutenbergWebsite;
+use Zareismail\Gutenberg\Nova\Website;
+
+return [ 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Gutenberg Resources Classe
+    |--------------------------------------------------------------------------
+    |
+    | This configuration option allows you to specify custom resources classe
+    | to use instead of the type that ships with Gutenberg. You may use this to
+    | define any extra form fields or other custom behavior as required.
+    |
+    */
+   
+	'resources' => [
+		'website' => Website::class, 
+	],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Gutenberg Resources Model Classes
+    |--------------------------------------------------------------------------
+    |
+    | This configuration option allows you to specify custom resources model
+    | to use instead of the type that ships with Gutenberg.
+    |
+    */
+   
+    'models' => [
+        Website::class => GutenbergWebsite::class, 
+    ],
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Gutenberg Supported Locales
+    |--------------------------------------------------------------------------
+    |
+    | This configuration option allows you to add or remove locale from Gutenberg website.
+    |
+    */
+   
+    'locales' => [ 
+        'en' => __('English'),
+    ], 
+];
