2085a34d17561d43b9b07b2ce24c268c6357d0fc
Ismail Zare <zarehesmaiel@gmail.com>
2022-03-24 21:27:57 +0430

    Added multiple default value support

--- src/Compilers/CompilesVariables.php
+++ src/Compilers/CompilesVariables.php
@@ -17,16 +17,57 @@ class CompilesVariables implements Compiler
      */
     public function compile(string $expression, array $attributes = [])
     {
-    	$pattern = "/\{\{\s*(?<variable>[0-9a-zA-Z-_.]+)(?:\s*or\s*(?<default>[^}]+))?\s*\}\}/"; 
+    	$pattern = "/\{\{\s*(?<variable>[0-9a-zA-Z-_.]+)(?:\s+or\s+(?<default>[^}]+))?\s*\}\}/"; 
 
         return preg_replace_callback($pattern, function($matches) use ($attributes) {   
-            $defaultValue = isset($matches['default']) 
-                ? data_get($attributes, trim($matches['default']), trim($matches['default']))
+            $defaultValue = isset($matches['default'])  
+                ? $this->getDefaultValue($attributes, trim($matches['default']))
                 : null;
 
             return strval(data_get(
                 $attributes, $matches['variable'], $defaultValue
             ));  
         }, $expression); 
+    }
+
+    /**
+     * get value of defaults.
+     * 
+     * @param  array $attributes 
+     * @param  string $defaultKey 
+     * @return mixed             
+     */
+    public function getDefaultValue($attributes, $defaultKey)
+    {
+        $defaults = $this->parseDefaults($defaultKey);
+        $defaultKey = $defaults->first(function($default) use ($attributes) {
+            return isset($attributes[$default]);
+        }, $defaults->pop());
+
+        if ($defaultKey === 'null' || 
+            $defaultKey === "''" || 
+            $defaultKey === '""'
+            ) {
+            return null;
+        }
+
+        return $attributes[$defaultKey] ?? $defaultKey;         
+    }
+
+    /**
+     * Parse default key string.
+     * 
+     * @param  string $defaultKey 
+     * @return array             
+     */
+    protected function parseDefaults($defaultKey)
+    {
+        $separator = md5($defaultKey);  
+        $defaultKey = preg_replace('/\s+or\s+/', $separator, $defaultKey);
+        $defaults = (array) explode($separator, $defaultKey); 
+
+        return collect($defaults)->map(function($default) {
+            return trim($default);
+        });
     }
 }
